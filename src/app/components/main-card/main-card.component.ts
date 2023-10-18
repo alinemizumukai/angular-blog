61a01a19ed368a85001263178cf2873ca7ab34d6
Aline Mizumukai <aline.mizumukai@mag-it.com.br>
2023-10-17 21:15:58 -0300

    feat: add routes and content component

--- src/app/components/main-card/main-card.component.ts
+++ src/app/components/main-card/main-card.component.ts
@@ -8,6 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MainCardComponent implements OnInit {
 
   @Input()
+  id:string = "0";
+  @Input()
   photoCover:string = "";
   @Input()
   cardTitle:string = "";
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": 1,
+        "photo": "https://m.media-amazon.com/images/M/MV5BZmVmMjdkNzctNGM0OC00MmE4LTlmYTktZTMyNzVhMjQ5MjhhXkEyXkFqcGdeQXVyNTM3MDMyMDQ@._V1_.jpg",
+        "title": "ABC renova Grey's Anatomy para 20ª temporada após saída de Ellen Pompeo",
+        "description": "Donec ex leo, dignissim id ultrices ut, pretium non purus. Nullam sollicitudin, ligula nec bibendum pellentesque, orci justo fringilla arcu, a sollicitudin enim ipsum ac lorem."
+    },
+    {
+        "id": 2,
+        "photo": "https://s.abcnews.com/images/GMA/greys-anatomy-tv-cast-01b-abc-llr-221004_1664912913205_hpMain_16x9_1600.jpg",
+        "title": "ABC revela trailer da Temporada 19",
+        "description": "Donec ex leo, dignissim id ultrices ut, pretium non purus. Nullam sollicitudin, ligula nec bibendum pellentesque, orci justo fringilla arcu, a sollicitudin enim ipsum ac lorem."
+    },
+    {
+        "id": 3,
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2uNhQjfcEH_db4Hhgi2ljtN5YUIAof_1MsA&usqp=CAU",
+        "title": "30 curiosidades que você talvez não soubesse sobre a série",
+        "description": "Donec ex leo, dignissim id ultrices ut, pretium non purus. Nullam sollicitudin, ligula nec bibendum pellentesque, orci justo fringilla arcu, a sollicitudin enim ipsum ac lorem."
+    },
+    {
+        "id": 4,
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV4KXo_dxC-ALeX7epVNBHvQpGYDG2fxUePXWApwpaan99mgyQWowpGrUpF5F7rkCjPcA&usqp=CAU",
+        "title": "Os 10 melhores personagens da série",
+        "description": "Donec ex leo, dignissim id ultrices ut, pretium non purus. Nullam sollicitudin, ligula nec bibendum pellentesque, orci justo fringilla arcu, a sollicitudin enim ipsum ac lorem."
+    }
+];
